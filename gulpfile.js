2eaac2b85e7472c7936bc4f1580e85a1c4ee9791
Rich <richardstiehm@gmail.com>
2016-12-09 17:54:19 -0500

    Styled Up to Our Begining

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,8 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
 
-//DEFINE DEFAULT TASK
-gulp.task('default', function(){
-	console.log("gulp task");
-});
 
-gulp.task("html", function(){
-	console.log("Imagine HTML TASK");
-});
 
-gulp.task("styles", function(){
-	return gulp.src("./app/assets/styles/styles.css")
-		   .pipe(postcss([cssImport, nested, cssvars, autoprefixer]))
-		   .pipe(gulp.dest('./app/temp/styles'));	
-});
 
-gulp.task("watch", function(){
-	watch('./app/index.html', function(){
-		gulp.start('html');
-	});
-	watch('./app/assets/styles/**/*.css', function(){
-		gulp.start('styles');
-	});
-});
+
