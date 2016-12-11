d41233ecd16d6505aa8952a0a323642b3d1baebb
Rich <richardstiehm@gmail.com>
2016-12-11 18:22:45 -0500

    Working NAV

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -16,6 +16,10 @@ gulp.task("watch", function(){
 	watch('./app/assets/styles/**/*.css', function(){
 		gulp.start('cssInject');
 	});
+
+	watch('./app/assets/scripts/**/*.js', function(){
+		gulp.start('scriptsRefresh');
+	})
 });
 
 
@@ -23,3 +27,12 @@ gulp.task("cssInject", ['styles'], function() {
 	return gulp.src('./app/temp/styles/styles.css')
 	.pipe(browserSync.stream());
 });
+
+gulp.task('scriptsRefresh', ['scripts'], function() {
+	browserSync.reload();
+});
+
+
+
+
+
